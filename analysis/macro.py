579af6d944d416357a52ce64df83f4ac26105f37
Dylan Terry-Doyle <dylanterrydoyle@gmail.com>
2026-03-12 14:21:20 +0000

    Update macro analysis

--- analysis/macro.py
+++ analysis/macro.py
@@ -7,7 +7,7 @@ from dynamin.utils import load_config, sql_engine
 from analysis.utils import load_macro_data, box_plot_scenarios
 
 # is this analysis from the examples folder?
-is_true = input('Run macro.py analysis for an example from the examples folder [y/n]: ').lower().startswith('y')
+is_true = input("Run macro.py analysis for an example from the examples folder [y/n]: ").lower().startswith("y")
 
 # if so, which example is this
 if is_true:
@@ -16,7 +16,7 @@ if is_true:
 ### Plot Parameters ###
 
 # change matplotlib font to serif
-plt.rcParams['font.family'] = ['serif']
+plt.rcParams["font.family"] = ["serif"]
 # figure size
 x_figsize = 10
 y_figsize = x_figsize/2
@@ -37,7 +37,7 @@ dynamin_path = Path(analysis_path).parent
 examples_path = dynamin_path / "examples"
 # figure path
 figure_path = analysis_path / "figures" / "macro"
-# create figure path if it doesn't exist
+# create figure path if it doesn"t exist
 figure_path.mkdir(parents=True, exist_ok=True)
 
 ### parameters ###
@@ -48,14 +48,13 @@ if is_true:
 else:
     params = load_config("parameters.yaml")
 # analysis parameters
-steps = params['simulation']['steps']
-num_years = params['simulation']['years']
-start = params['simulation']['start']*steps
-years = np.linspace(0, num_years, num_years*steps)
+steps = params["simulation"]["steps"]
+num_years = params["simulation"]["years"]
+start = params["simulation"]["start"] * steps
+years = np.linspace(0, num_years, num_years * steps)
 
 # random simulation 
-np.random.seed(params['simulation']['seed'])
-sim_index = np.random.randint(0, params['simulation']['num_sims'])
+sim_index = 5
 print("Randomly selected simulation index for plots: ", sim_index)
 
 # database parameters 
@@ -79,7 +78,11 @@ scenarios = pd.read_sql_query(
     engine
 )
 
-### start loop over databases ###
+### Plot GDP Ratios ###
+
+print(f"Creating GDP ratio plots...")
+
+# start loop over databases
 
 for i, scenario in scenarios.iterrows():
     # suffix for saving figures 
@@ -87,10 +90,6 @@ for i, scenario in scenarios.iterrows():
     
     # get macro data 
     macro_data = load_macro_data(engine, scenario["scenario_id"], params)
-        
-    ### Plot GDP Ratios ###
-
-    print(f"- creating GDP ratio figures")
 
     # average over simulations
     macro_group = macro_data.groupby(by="time")
@@ -103,31 +102,31 @@ for i, scenario in scenarios.iterrows():
     
     # 1) debt ratio
     ax0 = axes[0]
-    ax0.plot(years, macro_median["debt_ratio"], color='k', linewidth=1)
+    ax0.plot(years, macro_median["debt_ratio"], color="k", linewidth=1)
     ax0.plot(years, macro_data.loc[macro_data["simulation_index"]==sim_index, "debt_ratio"], linestyle="--", color="k", linewidth=1)
     ax0.fill_between(years, macro_lower["debt_ratio"], macro_upper["debt_ratio"], alpha=0.2, linestyle="--", color="tab:red", linewidth=1)
-    ax0.set_ylim((0.45, 2.15))
-    ax0.set_yticks((0.5, 1.0, 1.5, 2.0))
+    ax0.set_ylim((0.08, 0.72))
+    ax0.set_yticks((0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7))
     # 2) wage share
     ax1 = axes[1]
-    ax1.plot(years, macro_median["wage_share"], color='k', linewidth=1)
+    ax1.plot(years, macro_median["wage_share"], color="k", linewidth=1)
     ax1.plot(years, macro_data.loc[macro_data["simulation_index"]==sim_index, "wage_share"], linestyle="--", color="k", linewidth=1)
     ax1.fill_between(years, macro_lower["wage_share"], macro_upper["wage_share"], alpha=0.2, linestyle="--", color="tab:green", linewidth=1)
-    ax1.set_ylim((0.65, 0.97))
-    ax1.set_yticks((0.7, 0.8, 0.9))
+    ax1.set_ylim((0.73, 1.02))
+    ax1.set_yticks((0.75, 0.8, 0.85, 0.9, 0.95, 1.0))
     # 3) profit
     ax2 = axes[2]
-    ax2.plot(years, macro_median["profit_share"], color='k', linewidth=1)
+    ax2.plot(years, macro_median["profit_share"], color="k", linewidth=1)
     ax2.plot(years, macro_data.loc[macro_data["simulation_index"]==sim_index, "profit_share"], linestyle="--", color="k", linewidth=1)
     ax2.fill_between(years, macro_lower["profit_share"], macro_upper["profit_share"], alpha=0.2, linestyle="--", color="tab:blue", linewidth=1)
-    ax2.set_ylim((-0.05, 0.27))
-    ax2.set_yticks((0.0, 0.1, 0.2))
-    # tick size
+    ax2.set_ylim((-0.01, 0.26))
+    ax2.set_yticks((0.0, 0.05, 0.1, 0.15, 0.2, 0.25))
+    # tick size 
     for ax in axes:
         ax.tick_params(labelsize=fontsize)
     # save fig 
     plt.tight_layout()
-    plt.savefig(figure_path / f"gdp_shares_{suffix}", bbox_inches='tight')
+    plt.savefig(figure_path / f"gdp_shares_{suffix}", bbox_inches="tight")
 
 ### Plot Box Plots Scenarios ###
 
@@ -137,13 +136,28 @@ print("Creating box plots...")
 scenarios_short_names = ["G1", "G2", "ZG1", "ZG2"]
 
 # macro data for all scenarios
-macro_scenario_data = {short_name: load_macro_data(engine, scenario_id, params) for short_name, scenario_id in zip(scenarios_short_names, scenarios["scenario_id"])}
+macro_scenario_data = {
+    short_name: load_macro_data(engine, scenario_id, params) for short_name, scenario_id in zip(scenarios_short_names, scenarios["scenario_id"])
+}
 
 # figure settings
 xticks = [1, 2, 3, 4]
-colours = ['tab:blue', 'tab:blue', 'tab:green', 'tab:green']
+colours = ["tab:blue", "tab:blue", "tab:green", "tab:green"]
 
-macro_vars = ["rgdp_growth", "productivity_growth", "inflation", "wage_inflation", "unemployment_rate", "gini", "credit_gdp", "yearly_crisis_prob"]
+macro_vars = [
+    "rgdp_growth", 
+    "productivity_growth", 
+    "avg_loan_interest", 
+    "inflation", 
+    "wage_inflation", 
+    "unemployment_rate", 
+    "gini", 
+    "credit_gdp", 
+    "crisis_probability", 
+    "crisis_severity", 
+    "value_at_risk", 
+    "expected_shortfall"
+]
 
 for var in macro_vars:
     box_plot_scenarios(
